3a6bd81a20f4249ed359701ada084628eef3ef26
AdrianCooke <adrianmjcooke@googlemail.com>
2018-04-07 00:37:23 +0100

    Add project files.

---
+++ ConsoleApp.CodeInNeedOfFactoryPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp.CodeInNeedOfFactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
